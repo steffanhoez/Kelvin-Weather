0d034370f1fea9085560ad01527d9cb0488ad3cb
sofiane-hoez <62793274+sofiane-hoez@users.noreply.github.com>
2020-12-29 19:31:59 +0100

    Add files via upload

---
+++ Kelvin Weather.js
@@ -0,0 +1,13 @@
+// Kelvin start for today
+const kelvin = 0;
+
+// convert kelvin to celsius by subtracting
+const celsius = kelvin - 273;
+
+// convert celsius to fahrenheit ( fahrenheit calculate )
+let newton = celsius * (33 / 100);
+
+// fahrenheit decimal number
+fahrenheit = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees newton.`);
